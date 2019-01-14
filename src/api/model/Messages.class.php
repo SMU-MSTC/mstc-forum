1b1f40dbda1cb265696a6894016a1ecd3ab7c6c2
Tianqi <airstone42@gmail.com>
2019-01-14 14:40:55 +0800

    fix message display bug

--- src/api/model/Messages.class.php
+++ src/api/model/Messages.class.php
@@ -54,7 +54,7 @@ class Messages extends Model
 
     public function read($message_id)
     {
-        $message_query = "UPDATE messages SET message_is_read = true WHERE message_id='$message_id'";
+        $message_query = "UPDATE messages SET message_is_read=TRUE WHERE message_id='$message_id'";
         return pg_query($this->connection, $message_query) ? true : false;
     }
 
@@ -68,10 +68,6 @@ class Messages extends Model
     {
         if (isset($user_id)) {
             $notice = pg_fetch_all(pg_query($this->connection, "SELECT messages.*, users.user_name AS message_from_user_name,
-                                                                          users.user_is_admin AS message_from_user_is_admin FROM messages
-                                                                        LEFT JOIN users ON users.user_id=message_from
-                                                                      WHERE message_to='$user_id' AND message_type=FALSE ORDER BY message_id DESC"));
-            $message = pg_fetch_all(pg_query($this->connection, "SELECT messages.*, users.user_name AS message_from_user_name,
                                                                            replies.thread_id AS message_from_thread_id,
                                                                            threads.thread_title AS message_from_thread_title FROM messages
                                                                          LEFT JOIN users ON users.user_id=message_from
@@ -79,10 +75,18 @@ class Messages extends Model
                                                                            AND replies.reply_content=message_content AND replies.reply_time=message_time
                                                                          LEFT JOIN threads ON threads.thread_id=replies.thread_id
                                                                        WHERE message_to='$user_id' AND message_type=TRUE ORDER BY message_id DESC"));
+            $message = pg_fetch_all(pg_query($this->connection, "SELECT messages.*, users.user_name AS message_from_user_name,
+                                                                          users.user_is_admin AS message_from_user_is_admin FROM messages
+                                                                        LEFT JOIN users ON users.user_id=message_from
+                                                                      WHERE message_to='$user_id' AND message_type=FALSE ORDER BY message_id DESC"));
+            if (!$notice)
+                $notice = array();
+            if (!$message)
+                $message = array();
             $result = array_merge($notice, $message);
             foreach ($result as $key => $value)
                 $message_id[$key] = $value["message_id"];
-            array_multisort($message_id, SORT_DESC,$result);
+            array_multisort($message_id, SORT_DESC, $result);
             return $result;
         } else {
             return NULL;
